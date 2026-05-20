2b466dbe09ce34374f8a0cd397790d587819f137
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-05-20 18:48:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -162,6 +162,7 @@ class FLClient:
                     samples    = self.train_samples,
                     style_bank = active_style_bank,
                     client_id  = self.client_id,
+                    M          = M,
                     beta       = self.cfg["fft_beta"],
                     img_side   = img_side,
                     grayscale  = grayscale,
